397833e8f6302e2591a52fc1015e91127fe04ab4
Wecksten <mikael.wecksten@kits.se>
2017-11-26 23:35:19 +0100

    New all

---
+++ pelicanconf.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+THEME = 'tuxlite_tbs'
+
+AUTHOR = u'Weckzen'
+SITENAME = u'Weckzen garble'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+DEFAULT_PAGINATION = 4
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
